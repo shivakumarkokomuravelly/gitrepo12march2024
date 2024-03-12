1590726cb640a7480ef0088a76dfc3b1266c1461
shivakumarkomuravelly <shivakumarkomuravelly@gmail.com>
2024-03-12 07:03:07 +0530

    eigth commit: changed second.py for color change

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
 this is another functinality related to netbanking
 this is some additional functionality
 this is some other functionality added in netbanking 
+this is some other functionality done in netbanking to chage background color red 
